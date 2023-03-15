96843ab4048c0a95512d70defd85b44bb3d16801
Rafael <enterrafael@gmail.com>
2023-03-14 23:15:09 -0300

    Javadoc

---
+++ Exercicios/JavaDoc/JavadocTeste.java
@@ -0,0 +1,35 @@
+package JavaDoc;
+/**
+ * <code>JavadocTeste</code>
+* Returns an Image <i>object</i> that can then be painted on the screen.
+* The url argument must specify an absolute {@link URL}. The name
+* argument is a specifier that is relative to the url argument.
+* <p>
+* This method always returns immediately, whether or not the
+* image exists. ...
+* @version 1.3
+* @since 1.0
+* @author Rafael Florentino
+* @param url an absolute URL giving the base location of the image
+* @param name the location of the image, relative to the url argument
+* @return the image at the specified URL
+* 
+*/
+
+public class JavadocTeste {
+    /**
+     * Armazena valor
+     * @param valor
+     * @throws InterruptedException
+     */
+
+    /**
+     * Recupera valor
+     * @return valor
+     * @throws InterruptedException
+     */    
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+    
+}
