2fd512ea569aa2e19a31f2ef83d21a733fe75d5a
Rafael <enterrafael@gmail.com>
2023-03-21 20:45:28 -0300

    Lambda 2

---
+++ Exercicios/LambdaScopeTest.java
@@ -0,0 +1,38 @@
+/* 
+  Objetivo: Crie um programa que use expressoes laambda
+  Entrada: Sem entrada.
+  Saida:  Impreção dos dados na tela.
+  Autor: Rafael Florentino.
+*/
+import java.util.function.Consumer;
+
+public class LambdaScopeTest {
+    public int x = 0;
+
+    class PrimeiroNivel {
+        public int x = 1;
+
+        void metodoNoPrimeiroNivel(int x) {
+            int z = 2;
+            Consumer<Integer> meuConsumidor = (y) -> {
+                // A instrução a seguir faz com que o compilador gere
+                // o erro "Variável local z definida em um escopo delimitador
+                // deve ser final ou efetivamente final "
+                //
+                // z = 99;
+                System.out.println("x =" + x);
+                System.out.println("y =" + y);
+                System.out.println("z =" + z);
+                System.out.println("this.x =" + this.x);
+                System.out.println("LambdaScopeTest.this.x =" + LambdaScopeTest.this.x);
+            };
+            meuConsumidor.accept(x);
+        }
+    }
+
+    public static void main(String... args) {
+        LambdaScopeTest st = new LambdaScopeTest();
+        LambdaScopeTest.PrimeiroNivel fl = st.new PrimeiroNivel();
+        fl.metodoNoPrimeiroNivel(23);
+    }
+}
--- Exercicios/Lambdas.java
+++ Exercicios/Lambdas.java
@@ -1,16 +1,26 @@
+/* 
+  Objetivo: Crie um programa que use expressoes laambda
+  Entrada: Sem entrada.
+  Saida:  Impreção dos dados na tela.
+  Autor: Rafael Florentino.
+*/
 public class Lambdas {
     public static void main(String args[]) {
         Lambdas testador = new Lambdas();
+        
         // com declaração de tipos
         OperacaoMatematica adicao = (int a, int b) -> a + b;
+
         // sem declaração de tipos
         OperacaoMatematica subtracao = (a, b) -> a - b;
+
         // sem instrução de retorno com chaves
         OperacaoMatematica multiplicacao = (int a, int b) -> {
         return a * b;
     };
         // sem instrução de retorno e sem chaves
         OperacaoMatematica divisao = (int a, int b) -> a / b;
+
         // 10 + 5 = 15
         System.out.println("10 + 5 = " + testador.operar(10, 5, adicao));   
         // 10 - 5 = 5
@@ -19,12 +29,15 @@ public class Lambdas {
         System.out.println("10 x 5 = " + testador.operar(10, 5, multiplicacao));
         // 10 / 5 = 2
         System.out.println("10 / 5 = " + testador.operar(10, 5, divisao));
+
         // sem parênteses
         ServicoCumprimento servicoCumpr1 =
         message -> System.out.println("Olá " + message);
+
         // com parênteses
         ServicoCumprimento servicoCumpr2 =
-        (message) -> System.out.println("Olá " + message);          
+        (message) -> System.out.println("Olá " + message); 
+
         // Olá Paulo
         servicoCumpr1.dizerMensagem("Paulo");
         // Olá Maria
